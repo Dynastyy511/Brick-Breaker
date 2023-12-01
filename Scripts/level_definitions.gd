1104160f4c6fc0d2fc9f8096c71b5f7f750fcce5
machen18 <machen18@s.sfusd.edu>
2023-12-01 14:28:55 -0800

    level 3

--- Scripts/level_definitions.gd
+++ Scripts/level_definitions.gd
@@ -5,8 +5,12 @@ var current_level = 1
 var level_1 = [
 	[0,1,0]
 ]
-
 var level_2 = [
+	[2,2,2,2,2],
+	[0,3,3,3,0],
+	[0,0,4,0,0]
+]
+var level_3 = [
 	[5,5,5,5,5,5,5,5],
 	[5,5,4,4,4,5,5,5],
 	[0,0,5,5,5,3,0,0],
@@ -14,7 +18,7 @@ var level_2 = [
 	[1,1,1,1,1,1,1,1]
 ]
 
-var levels = [level_1, level_2]
+var levels = [level_1, level_2, level_3]
 
 func get_current_level():
 	print(levels[current_level - 1])
